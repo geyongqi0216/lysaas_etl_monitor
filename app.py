57c9bc92184ce740c26b0791b5b8a0ec81f6b943
曉楓 <xiaofeng@luoyuit.com>
2020-11-10 17:38:17 +0800

    页面开发

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect,url_for
 app = Flask(__name__)
 
 # 视图函数 总览页面
 
 
-@app.route("/")
+@app.route("/etlmonitor")
+def default():
+    return redirect('/etlmonitor/index')
+
+@app.route("/etlmonitor/index")
 def index():
     #pendect()
     return render_template('myindex.html', table_name='app_order')
 
 
-@app.route("/datasync-topic")
+@app.route("/etlmonitor/datasync-topic")
 def datasync_topic():
     return render_template('datasync-topic.html')
+
+@app.route("/etlmonitor/datasync-table")
+def datasync_table():
+    return render_template('datasync-table.html')
 
 
 if __name__ == "__main__":
